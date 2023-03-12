808c03606a32b27faa845ce6348c47eba20e7c99
Dilnaz <dilnaz@stanford.edu>
2023-03-12 14:59:34 -0700

    led.c

---
+++ led.c
@@ -0,0 +1,12 @@
+#include "spi.h"
+
+// SPI parameters
+#define POL     0
+#define PHA     0
+#define CLKDIV  40
+
+
+void led_init(void) {
+    spi_init(POL, PHA, CLKDIV);
+    printf("spi initialized!\n");
+}
