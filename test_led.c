3abd7aa54dd4bb62fe6c0c41ae87b44b5163587a
Dilnaz <dilnaz@stanford.edu>
2023-03-13 14:31:15 -0700

    testing

--- test_led.c
+++ test_led.c
@@ -1,10 +1,18 @@
-#include "uart.h"
+#include "led.h"
 #include "mymodule.h"
+#include "spi.h"
+#include "uart.h"
+
+
 
 void main(void)
 {
     uart_init();
-    hello("CS107e");
-    //led_init();
+    led_init();
+    printf("LED strip initialized!\n");
+
+    clear_strip(255, 0, 0);
+    led_show();
+
     uart_putchar(EOT);
 }
