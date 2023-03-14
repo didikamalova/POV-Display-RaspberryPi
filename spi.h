9219aafb7aee7abc60a5832b242fb3b991a5a21f
Dilnaz <dilnaz@stanford.edu>
2023-03-13 22:16:58 -0700

    fixing spi.h

--- spi.h
+++ spi.h
@@ -4,14 +4,4 @@
 void spi_init(unsigned pol, unsigned pha, unsigned clk_div);
 void spi_txrx(unsigned char* txbuf, unsigned char* rxbuf, unsigned len);
 
-<<<<<<< HEAD
 #endif
-=======
-#define SPI_CE0 0
-#define SPI_CE1 1
-
-void spi_init(unsigned chip_select, unsigned clock_divider);
-void spi_transfer(unsigned char *tx, unsigned char *rx, unsigned len);
-
-#endif /* SPI_H */
->>>>>>> 97f40e92a04ec85177348cd2454db0769749843d
