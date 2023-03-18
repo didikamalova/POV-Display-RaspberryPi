2b51f0ef02e98f5c6c408556f65134e58e95c683
Naomi Mo <nmo@stanford.edu>
2023-03-17 17:41:51 -0700

    Testing main - switch between RGB on hall event.

--- main.c
+++ main.c
@@ -15,25 +15,40 @@ void main(void)
 	hall_init();
   	apa102_init();
 
-	int something = hall_read_event();
-    apa102_clear(255, 255, 0, 0);
-	printf("LED strip driven blue.\n");
-    apa102_show();
+//	int something = hall_read_event();
+//    apa102_clear(255, 255, 0, 0);
+//	printf("LED strip driven blue.\n");
+  //  apa102_show();
 
-	int another = hall_read_event();
-	apa102_clear(0,255,0,0);
-	printf("LED strip off.\n");
-	apa102_show();
+//	int another = hall_read_event();
+//	apa102_clear(0,255,0,0);
+//	printf("LED strip off.\n");
+//	apa102_show();
 
-	int ungabunga = hall_read_event();
-	apa102_clear(255, 0, 255, 0);
-	printf("GREEN.\n");
-	apa102_show();
+//	int ungabunga = hall_read_event();
+//	apa102_clear(255, 0, 255, 0);
+//	printf("GREEN.\n");
+//	apa102_show();
 
-	int ungabunga2 = hall_read_event();
-	apa102_clear(255, 0, 0, 255);
-	apa102_show();
+//	int ungabunga2 = hall_read_event();
+//	apa102_clear(255, 0, 0, 255);
+//	apa102_show();
 
+	int count = 0;
+	while(1) {
+		hall_read_event();
+		if (count == 0) {
+			apa102_clear(255,255,0,0);
+			count++;
+		} else if (count == 1) {
+			apa102_clear(255, 0, 0, 255);
+			count++;
+		} else {
+			apa102_clear(255, 0, 255, 0);
+			count = 0;
+		}
+		apa102_show();
+	}
     uart_putchar(EOT);
 
 }
