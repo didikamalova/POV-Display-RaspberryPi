2a5e80f71a3978b715fcde8f043e2e9e9516dc94
Naomi Mo <nmo@stanford.edu>
2023-03-24 12:33:58 -0700

    Testing globe implementation post-demo

--- main.c
+++ main.c
@@ -7,54 +7,36 @@
 void main(void)
 {
     uart_init();
-    //hall_init();
+    hall_init();
     apa102_init();
 
-    for (int i = 0; i < 20; i++) {
-        apa102_clear(BLUE);
-        apa102_show();
-        timer_delay_ms(300);
-        apa102_clear(RED);
-        apa102_show();
-        timer_delay_ms(300);
-        apa102_clear(GREEN);
-        apa102_show();
-        timer_delay_ms(300);
-    }
+//    for (int i = 0; i < 20; i++) {
+//        apa102_clear(BLUE);
+//       apa102_show();
+//        timer_delay_ms(300);
+//       apa102_clear(RED);
+//        apa102_show();
+//        timer_delay_ms(300);
+//        apa102_clear(GREEN);
+//        apa102_show();
+//        timer_delay_ms(300);
+//    }
 	
-	// commented out code for hall-sensor driving testing
-
-//	int another = hall_read_event();
-//	apa102_clear(0,255,0,0);
-//	printf("LED strip off.\n");
-//	apa102_show();
-
-//	int ungabunga = hall_read_event();
-//	apa102_clear(255, 0, 255, 0);
-//	printf("GREEN.\n");
-//	apa102_show();
-
-//	int ungabunga2 = hall_read_event();
-//	apa102_clear(255, 0, 0, 255);
-//	apa102_show();
-
-	apa102_clear(RED);
-	apa102_show();
-
-//	int count = 0;
-//	while(1) {
-//		hall_read_event();
-//		if (count == 0) {
-//			apa102_clear(RED);
-//			count++;
-//		} else if (count == 1) {
-//			apa102_clear(BLUE);
-//			count++;
-//		} else {
-//			apa102_clear(GREEN);
-//			count = 0;
-//		}
-//		apa102_show();
-//	}
+	int count = 0;
+	while(1) {
+		hall_read_event();
+		if (count == 0) {
+			apa102_clear(BLUE);
+			count++;
+		} //else if (count == 1) {
+			//apa102_clear(BLUE);
+		//	count++;
+		//} 
+		else {
+			apa102_clear(GREEN);
+			count = 0;
+		}
+		apa102_show();
+	}
     uart_putchar(EOT);
 }
