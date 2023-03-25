a28891ed4c8f0c66044e73faef4106567397dabd
Aanya Tashfeen <aanyatash@gmail.com>
2023-03-24 21:33:47 -0700

    fixed constants

--- constants.h
+++ constants.h
@@ -2,14 +2,10 @@
 
 
 #define VERTICAL_RESOLUTION 60 // no. of LEDs on each side
-#define HORIZONTAL_RESOLUTION 120 // calculate this by doing math - circumference/width of LED strip
-#define CHANNELS_PER_PIXEL 4 // RGBA
+#define HORIZONTAL_RESOLUTION 120 // calculate this by doing math - circumference of circular frame/width of LED strip
+#define CHANNELS_PER_PIXEL 4 // ABGR
 
 #define NUM_MAGNETS 6
-
-// can add gpio pin numbers for spi clock etc, and hall sensor
-// add brightness level as well
-
 
 #define NUM_PIXELS (VERTICAL_RESOLUTION*HORIZONTAL_RESOLUTION) // size in pixels/LEDs
 #define BUFFER_SIZE_PER_COLUMN (VERTICAL_RESOLUTION*CHANNELS_PER_PIXEL)
