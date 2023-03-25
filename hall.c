cba6a871681fe4b94f67070379a6ac1fa711dc9f
Naomi Mo <nmo@stanford.edu>
2023-03-24 22:07:14 -0700

    Added additional hall comments.

--- hall.c
+++ hall.c
@@ -27,11 +27,13 @@ volatile int which_magnet;
 
 rb_t *rb;
 
+/*
+ * Initialize the hall sensor; enable gpio and global interrupts.
+ */
 void hall_init(void) {
 	rb = rb_new();
 	which_magnet = 0;
 
-	//printf("initialize interrupts completed\n");
 	gpio_set_input(hall_pin);
   	gpio_set_function(hall_pin, GPIO_FUNC_INPUT);
   	gpio_set_pullup(hall_pin);
@@ -44,30 +46,37 @@ void hall_init(void) {
 	interrupts_global_enable();
 }
 
+/*
+ * Interrupts handler function for reading a hall event.
+ */
 void handle_hall(unsigned int pc, void *aux_data) {
 	rb_t *rb = (rb_t *) aux_data;
 	if (gpio_check_and_clear_event(hall_pin)) {
 		magnetevent = 1;
+
+		// Debouncing: only queue a magnet event if the previous state is low
 		if (lastmagnetevent == 0) {
 			magnetnum++;
 			lastmagnetevent = 1;
-	
-			// change comparison val depending on how many magnets we use
 			if (magnetnum > totalmagnets) magnetnum = 1;
-
 			rb_enqueue(rb, magnetnum);
 			which_magnet = magnetnum;
 		}
 	}
 }
 
+/*
+ * Function intended to track which magnet around that hall sensor's path that we are reading;
+ * initially written for debugging
+ */
 volatile int get_which_magnet(void) {
-    //printf("get which magnet after avg: %d\n", which_magnet);
 	lastmagnetevent = 0;
     return which_magnet;
 }
 
-// Read a hall event w/ debouncing implemented
+/*
+ * Read a hall event; spin while there are no hall events queued.
+ */
 int hall_read_event(void) {
 	while(rb_empty(rb)) {/* spin */};
 	rb_dequeue(rb, &which_magnet);
